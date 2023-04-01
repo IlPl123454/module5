b2e0d1baea8218ba056deb447e80d382fc251edc
Ilya P <ilplenkov@mail.ru>
2023-04-01 13:02:11 +0600

    first version

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace project
+{
+
+    class Program
+    {
+
+        public static void Main()
+        {
+            System.Console.WriteLine("Hello!!!");
+        }
+
+    }
+
+}
